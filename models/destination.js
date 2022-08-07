bc548a01db0c73bc47224a8e74e8ab2654327f23
Chyanne Robbins <chyannetech@gmail.com>
2022-08-06 23:35:13 -0400

    Fresh Slate

---
+++ models/destination.js
@@ -0,0 +1,23 @@
+module.exports = [{
+    Location:'Miami, FL',
+    Image:'https://i.imgur.com/hV0Rzsm.jpg'
+},{
+    Location:'Fort Lauderdale, FL',
+    Image:'https://i.imgur.com/wUdp2Zc.jpg',
+},{
+    Location:'Brooklyn',
+    Image:'https://i.imgur.com/dRvEEG3.jpg',
+},{
+    Name:'Denver, CO',
+    Image:'https://i.imgur.com/rZ56ery.jpg',
+},
+{
+    Name:'Phoenix, AZ',
+    Image:'https://i.imgur.com/HB3WVEW.jpg',
+},
+{
+    Name:'Tampa',
+    Image:'blob:https://imgur.com/95bd32b8-22f5-4d9d-92de-8dc36e3104f8',
+}];
+
+console.log(module);
